c3889c175645a4e7b8b4027c26ff726c151b7d6f
riaasrini17-debug <riaasrini17@gmail.com>
2026-05-25 20:23:59 +0700

    membuat halaman login untuk admin dan owner

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+
+
+            if ($user->role === 'owner') {
+                return redirect()->route('owner.dashboard');
+            } 
+
+            elseif ($user->role === 'admin') {
+                return redirect()->route('dashboard');
+            }
+            return redirect()->route('dashboard');
+        }
+
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah, silakan coba lagi.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        
+        return redirect('/login');
+    }
+}
