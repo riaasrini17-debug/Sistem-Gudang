2ec0b9500962afcaea344f9c4b0b5c2cac36250e
dinaypurnami-ship-it <dinaypurnami@gmail.com>
2026-07-07 23:04:08 +0700

    fix: memperbaharui katagori

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -9,15 +9,13 @@ class Kategori extends Model
 {
     use HasFactory;
 
-    // Tentukan nama tabel jika Laravel salah menebak jamak (plural) nya
+
     protected $table = 'kategori';
 
-    // Kolom yang diizinkan untuk diisi massal
+    
     protected $fillable = ['nama_kategori'];
 
-    /**
-     * Relasi: Satu kategori memiliki banyak barang (One to Many)
-     */
+
     public function barangs()
     {
         return $this->hasMany(Barang::class, 'kategori_id');
